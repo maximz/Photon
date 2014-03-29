8be9a5d1e1858feb11e7a0a28b04e3dc5d9342cf
Chris Piller <cgpiller@gmail.com>
2014-03-29 19:52:35 -0400

    more with camera stuff for android

--- Android side/Photon/src/com/comparty/photon/CameraFragment.java
+++ Android side/Photon/src/com/comparty/photon/CameraFragment.java
@@ -8,6 +8,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
@@ -16,8 +19,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.comparty.photon.camera.CameraPreview;
 
@@ -37,7 +40,7 @@ public class CameraFragment extends Fragment {
 		FrameLayout preview = (FrameLayout) rootView.findViewById(R.id.camera_preview);
 		preview.addView(mCameraPreview);
 
-		Button captureButton = (Button) rootView.findViewById(R.id.button_capture);
+		ImageButton captureButton = (ImageButton) rootView.findViewById(R.id.button_capture);
 		captureButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -72,9 +75,17 @@ public class CameraFragment extends Fragment {
 				return;
 			}
 			try {
+				Bitmap bmp = BitmapFactory.decodeByteArray(data , 0, data.length);
+				
+				Matrix matrix = new Matrix();
+			    matrix.postRotate(90);
+			    bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+				
 				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
+				bmp.compress(Bitmap.CompressFormat.JPEG, 85, fos);
+		        fos.flush();
 				fos.close();
+				bmp.recycle();
 			} catch (FileNotFoundException e) {
 
 			} catch (IOException e) {
