6fc0e34dfa4628475d7a9b700a02ac693c64297b
Mike Zhang <mzhang6@gmail.com>
2014-03-29 02:01:49 -0400

    Added a web interface to app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
-var tesseract = require('./server_methods/tesseract_call.js').execute('image.png');
+// Takes one parameter: the word
+var tesseract = require('./server_methods/tesseract_call.js').execute;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendfile("index.html"); 
+});
+
+app.post('/web', function(req, res) {
+  res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+      , req.files.image.name
+      , req.files.image.size / 1024 | 0
+      , req.files.image.path
+      , req.body.title));
+}
 
 var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
